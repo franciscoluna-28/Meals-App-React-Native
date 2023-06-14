a11dc4eab0b005033ef0c98e12e19dc647913eb2
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-13 21:29:01 -0400

    Added styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+   content: [],
+   content: ["./App.{js,jsx,ts,tsx}", "./**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+        primary:'#FF6464'
+        }
+      },
+    },
+    plugins: [],
+  }
